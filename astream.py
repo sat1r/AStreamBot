cbce5e24e2bb3d8c5102c770803a3ced4fff5daf
Maks Zylicki <m_zylicki@Makss-MacBook-Pro.local>
2017-07-30 23:06:28 +0300

    Adjust buildpacks on Heroku

--- astream.py
+++ astream.py
@@ -1,5 +1,6 @@
-import astreamBotHandler
+from astreamBotHandler import *
 
+token = '440415992:AAH3llC2JW4L4zgoVphgVj98-kIWw_5sW0Y'
 greet_bot = BotHandler(token)  
 greetings = ('hello', 'hi', 'greetings', 'sup')  
 now = datetime.datetime.now()
